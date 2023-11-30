0aadbbee0ca8c74f4c098bf050ec408db9936039
jooo71 <93451542+jooo71@users.noreply.github.com>
2023-11-30 21:01:26 +0200

    Add files via upload

--- main.dart
+++ main.dart
@@ -9,7 +9,10 @@ import 'package:fl_chart/fl_chart.dart';
 void main() {
   runApp(const CupertinoApp(
     title: 'Navigation Basics',
+    debugShowCheckedModeBanner:false,
+
     home: FirstRoute(),
+
   ));
 }
 
@@ -48,14 +51,23 @@ class SecondRoute extends StatefulWidget {
 class _SecondRouteState extends State<SecondRoute> {
   List<List<dynamic>> _data = [];
   int button = 0;
+  bool showChart = false;
 
   void _loadCSV() async {
-    final _rawData = await rootBundle.loadString("assets/car-sales-joo.csv");
+    final _rawData = await rootBundle.loadString("assets/ECG.csv");
     List<List<dynamic>> _listData =
-        const CsvToListConverter().convert(_rawData);
+    const CsvToListConverter().convert(_rawData);
     setState(() {
       _data = _listData;
       button = (button == 0) ? 1 : 0; // Toggle the button state
+    });
+  }
+
+  void _toggleChart() {
+    setState(() {
+      showChart = !showChart;
+      button=0;
+      // print(showChart);
     });
   }
 
@@ -69,50 +81,71 @@ class _SecondRouteState extends State<SecondRoute> {
         padding: const EdgeInsets.all(16.0), // Add padding for spacing
         child: Stack(
           children: [
+            if (showChart)
+              Container(
+                height: 680,
+                child: LineChart(
+                  LineChartData(
+                    gridData: FlGridData(show: true),
+                    titlesData: FlTitlesData(show: false),
+                    borderData: FlBorderData(show: false),
+                    lineBarsData: [
+                      LineChartBarData(
+                        spots: _data
+                            .skip(1)
+                            .map(
+                              (row) => FlSpot(
+                            row[0].toDouble(),
+                            row[1].toDouble(),
+                          ),
+                        )
+                            .toList(),
+                        isCurved: true,
+                        color: Colors.blue,
+                        dotData: FlDotData(show: false),
+                        belowBarData: BarAreaData(show: false),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: CupertinoButton(
+                onPressed: _toggleChart,
+                child: const Text('Plot'),
+                color: Colors.red,
+              ),
+            ),
             if (button == 1)
-              ListView.builder(
-                itemCount: _data.length,
-                itemBuilder: (_, index) {
-                  return Card(
-                    margin: const EdgeInsets.all(3),
-                    color: index == 0 ? Colors.amber : Colors.white,
-                    child: ListTile(
-                      leading: Text(_data[index][0].toString()),
-                      title: Text(_data[index][1].toString()),
-                      trailing: Text(_data[index][2].toString()),
-                    ),
-                  );
-                },
+              Container(
+                height: 680,
+                // margin: Top;
+                child: ListView.builder(
+                  itemCount: _data.length,
+                  itemBuilder: (_, index) {
+                    return Card(
+                      margin: const EdgeInsets.all(3),
+                      color: index == 0 ? Colors.amber : Colors.white,
+                      child: ListTile(
+                        leading: Text(_data[index][0].toString()),
+                        title: Text(_data[index][1].toString()),
+                        trailing: Text(_data[index][2].toString()),
+                      ),
+                    );
+                  },
+                ),
               ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: CupertinoButton(
+                onPressed: () {
+                  _loadCSV();
+                  showChart = false;
 
-            Align(
-              alignment: Alignment
-                  .bottomRight, // Align the "Go back" button to the bottom-right corner
-              child: CupertinoButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text('Go back!'),
-              ),
-            ),
-            Align(
-              alignment: Alignment
-                  .bottomCenter, // Align the "Read CSV" button to the bottom-left corner
-              child: CupertinoButton(
-                onPressed: () {
-                   // Call the function to read the CSV file and toggle visibility
-                },
-                child: const Text('plot'),
-              ),
-            ),
-            Align(
-              alignment: Alignment
-                  .bottomLeft, // Align the "Read CSV" button to the bottom-left corner
-              child: CupertinoButton(
-                onPressed: () {
-                  _loadCSV(); // Call the function to read the CSV file and toggle visibility
                 },
                 child: const Text('Read CSV'),
+                color: Colors.red,
               ),
             ),
           ],
