29db1d7fbb53736c05dd020b2421e1a2579df4c1
jooo71 <93451542+jooo71@users.noreply.github.com>
2023-12-09 20:15:58 +0200

    ecg_monitor

--- main.dart
+++ main.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:csv/csv.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -6,36 +10,209 @@ import 'package:flutter/cupertino.dart';
 
 import 'package:fl_chart/fl_chart.dart';
 
-void main() {
-  runApp(const CupertinoApp(
-    title: 'Navigation Basics',
-    debugShowCheckedModeBanner:false,
+import 'package:firebase_core/firebase_core.dart';
+import 'package:path_provider/path_provider.dart';
 
-    home: FirstRoute(),
+import 'firebase_options.dart';
 
-  ));
+/*48aaaaaaal*/
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FirstRoute(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
 
 class FirstRoute extends StatelessWidget {
-  const FirstRoute({super.key});
+  const FirstRoute({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.black,
       navigationBar: const CupertinoNavigationBar(
         middle: Text('First Route'),
       ),
-      child: Center(
-        child: CupertinoButton(
-          child: const Text('Open route'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              CupertinoPageRoute(builder: (context) => const SecondRoute()),
-            );
-          },
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: ElevatedButton(style: ElevatedButton.styleFrom(backgroundColor: Colors.red, shape: StadiumBorder()),
+                    // color: Colors.green,
+                    // borderRadius: BorderRadius.circular(25),
+                    child: const Text('Firebase Button'),
+                    onPressed: () {
+                      // Load CSV data in CsvScreen when left button is pressed
+                      Navigator.push(
+                        context,
+                        CupertinoPageRoute(
+                          builder: (context) => CsvScreen(),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: ElevatedButton(style: ElevatedButton.styleFrom(backgroundColor: Colors.red, shape: StadiumBorder()),
+                    // color: Colors.cyan,
+                    // borderRadius: BorderRadius.circular(25),
+                    child: const Text('Open route'),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        CupertinoPageRoute(
+                            builder: (context) => const SecondRoute()),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class CsvScreen extends StatefulWidget {
+  @override
+  _CsvScreenState createState() => _CsvScreenState();
+}
+
+class _CsvScreenState extends State<CsvScreen> {
+  List<List<dynamic>> data = [];
+  @override
+  void initState() {
+    super.initState();
+    loadCsv();
+  }
+
+  Future<void> loadCsv() async {
+    try {
+      final storage = FirebaseStorage.instance;
+      final csvDataRef = storage.ref('csv/ECG.csv');
+      final dataBytes = await csvDataRef.getData();
+
+      if (dataBytes == null) {
+        // Handle the case where the data is null
+        print('CSV data is null.');
+        return;
+      }
+
+      final csvData = String.fromCharCodes(dataBytes);
+      final decoded = CsvToListConverter().convert(csvData);
+      setState(() {
+        data = decoded;
+      });
+    } catch (error) {
+      print('Error loading CSV: $error');
+      // Handle error, e.g., show an error message to the user
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Error'),
+            content: Text('Failed to load CSV data. Please try again.'),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CSV Data'),
+      ),
+      body: data.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    // height: 680,
+                    // child: ListView.builder(
+                    //   itemCount: data.length,
+                    //   itemBuilder: (_, index) {
+                    child: Card(
+                      margin: const EdgeInsets.all(3),
+                      color: index == 0 ? Colors.amber : Colors.white,
+                      child: ListTile(
+                        leading: Text(data[index][0].toString()),
+                        title: Text(data[index][1].toString()),
+                        trailing: Text(data[index][2].toString()),
+                      ),
+                      // );
+                      // },
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Container(
+            // height: 680,
+            child: Expanded(
+              child: LineChart(
+                LineChartData(
+                  gridData: FlGridData(show: true),
+                  titlesData: FlTitlesData(show: false),
+                  borderData: FlBorderData(show: false),
+                  lineBarsData: [
+                    LineChartBarData(
+                      spots: data
+                          .skip(1)
+                          .map(
+                            (row) => FlSpot(
+                          row[0].toDouble(),
+                          row[1].toDouble(),
+                        ),
+                      )
+                          .toList(),
+                      isCurved: true,
+                      color: Colors.blue,
+                      dotData: FlDotData(show: false),
+                      belowBarData: BarAreaData(show: false),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -66,19 +243,18 @@ class _SecondRouteState extends State<SecondRoute> {
   void _toggleChart() {
     setState(() {
       showChart = !showChart;
-      button=0;
-      // print(showChart);
+      button = 0;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: const CupertinoNavigationBar(
+      navigationBar: CupertinoNavigationBar(
         middle: Text('Second Route'),
       ),
       child: Padding(
-        padding: const EdgeInsets.all(16.0), // Add padding for spacing
+        padding: const EdgeInsets.all(16.0),
         child: Stack(
           children: [
             if (showChart)
@@ -120,7 +296,6 @@ class _SecondRouteState extends State<SecondRoute> {
             if (button == 1)
               Container(
                 height: 680,
-                // margin: Top;
                 child: ListView.builder(
                   itemCount: _data.length,
                   itemBuilder: (_, index) {
@@ -142,7 +317,6 @@ class _SecondRouteState extends State<SecondRoute> {
                 onPressed: () {
                   _loadCSV();
                   showChart = false;
-
                 },
                 child: const Text('Read CSV'),
                 color: Colors.red,
